30330d041ea86ed11cf473f3e99ff64a9a45c4b1
Darkenigma19 <arksingh74@gmail.com>
2025-04-18 23:50:29 +0530

    Headers Updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,48 @@
-'use client'
+"use client";
 
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
+import {
+  FaDiscord,
+  FaRedditAlien,
+  FaTelegramPlane,
+  FaTwitter,
+} from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 function Header() {
   const router = useRouter();
+  const handleSiderbar = () => {};
 
   return (
     <header className="sticky top-0 left-0 right-0 z-50 backdrop-blur-sm">
       <div className="absolute inset-0 opacity-60"></div>
 
       <div className="relative container mx-auto px-4 flex items-center justify-between">
-      <GiHamburgerMenu />
+        <button onClick={handleSiderbar}>
+          <GiHamburgerMenu />
+        </button>
+        <Link href={``}>
+          <FaDiscord />
+        </Link>
+        <Link href={``}>
+          <FaTelegramPlane />
+        </Link>
+        <Link href={``}>
+          <FaRedditAlien />
+        </Link>
+        <Link href={``}>
+          <FaTwitter />
+        </Link>
+
         <h1
           className="text-center text-5xl py-2 font-black hover:cursor-pointer"
           onClick={() => {
             router.push("/");
           }}
         >
-          Phantom 
-          
-          TV
+          Phantom TV
         </h1>
       </div>
     </header>
